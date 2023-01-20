f6a3c2a4b941f905e22e8266d0387d7d1bd23421
Raimund Canzler <git@rai-canzler.de>
2023-01-20 16:19:45 +0100

    wip: filter more stuff

--- extract.js
+++ extract.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const fs = require('fs');
-const { json } = require('stream/consumers');
 const _ = require('underscore');
+const centroid = require('@turf/centroid').default;
 
 const rawData = fs.readFileSync('./playgrounds.geojson');
-const osmData = JSON.parse(rawData);
+const geoJson = JSON.parse(rawData);
 
 const schema = {
     oid: "id",
@@ -16,16 +16,32 @@ const schema = {
     opening_hours: ["properties", "opening_hours"],
     wheelchair: ["properties", "wheelchair"],
     website: ["properties", "website"],
-    geometry: "geometry"
+    location: "geometry"
 }
 
 const playgrounds = [];
 
-for (const osmObj of osmData.features) {
-    const playground = {};
+for (const feature of geoJson.features) {
+    const playground = {
+        toys: []
+    };
 
     for (const key of Object.keys(schema)) {
-        playground[key] = _.get(osmObj, schema[key], null);
+        playground[key] = _.get(feature, schema[key], null);
+    }
+
+    // Extract point
+    if(playground.location.type === "Point") {
+        playground.location = playground.location.coordinates;
+    } else if (playground.location.type === "Polygon") {
+        const centerPoint = centroid(playground.location);
+        playground.location = centerPoint.geometry.coordinates;
+    }
+
+    // Extract playground toys
+    const featureToys = Object.keys(feature.properties).filter(key => key.startsWith('playground:') && feature[key] === "yes");
+    for (const toy of featureToys) {
+        playground.toys.push(toy.split(':')[1]);
     }
 
     playgrounds.push(playground)
